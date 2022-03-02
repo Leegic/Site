d712e2ed942e81da8d0d9d44960137f427030859
Rileey <ratzptr@hotmail.com>
2022-03-02 16:14:17 -0500

    files
    files of 1st site

---
+++ Website/js/website.js
@@ -0,0 +1,13 @@
+//music ideas
+//const music = new Audio('DeathGrip.mp3');
+//music.play();
+//music.loop=true;
+//music.playbackRate=5;
+//music.pause();
+
+var myString = "Hello Everybody";
+
+function displayStringPopup(){
+    window.alert(myString)
+}
+displayStringPopup();
